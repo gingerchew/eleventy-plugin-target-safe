0b12e0b3a0eeb4c5d82affc361c2583d6e686e7e
Frankie <26461046+frankie-tech@users.noreply.github.com>
2022-10-20 13:22:27 -0400

    Actual 11ty plugin file

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,40 @@
 // Example use for the demo plugin:
 // {{ 'Steph' | hello | safe }}
+const { Transformer } = require('./index.js');
 
-module.exports = (eleventyConfig, options) => {
+const now = () => {
+  const [ s, ns ] = process.hrtime();
+  const now = s * 1000 + ns / 1000000
+
+  return parseInt(now.toFixed(3));
+}
+
+module.exports = (eleventyConfig, options = {}) => {
   // Define defaults for your plugin config
   const defaults = {
-    htmlTag: "h2",
+    opener: true, // add the opener rel?
+    follower: false, // add the follower rel?
+    referrer: false, // add the referrer rel?
   };
+  
+  // By default it adds to any <a> or <form>
+  // with target="_blank|_self|_*|custom-id"
+  
+  // If follower is true, then any links that
+  // don't match the url will have the follower
+  // rel added to them
+
+  // referrer rel can be considered as a "strict" mode?
+  // might need to do more research on the *exact*
+  // implications of that one
+
+  const _options = Object.assign({}, defaults, options);
 
   // You can create more than filters as a plugin, but here's an example
-  eleventyConfig.addFilter("hello", (name) => {
-    // Combine defaults with user defined options
-    const { htmlTag } = {
-      ...defaults,
-      ...options,
-    };
-
-    return `<${htmlTag}>Hello, ${name}!</${htmlTag}>`;
+  eleventyConfig.addTransform("safer-rel", (content, outputPath) => {
+    const start = now();
+    const transformedContent = Transformer(content, _options);
+    console.log('[Safer Rel] Transformation completed in ' + (now() - start) + 'ms');
+    return transformedContent;
   });
 };
