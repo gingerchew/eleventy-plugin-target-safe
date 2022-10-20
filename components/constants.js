d71cf713be06583c6934ee91c57d1f9a958d7630
Frankie <26461046+frankie-tech@users.noreply.github.com>
2022-10-20 13:19:52 -0400

    Creates a file to store all constants

---
+++ components/constants.js
@@ -0,0 +1,16 @@
+const RELVALUES = [
+    "noopener",
+    "nofollower",
+    "noreferrer"
+];
+
+const SAFETARGETS = [
+    "_parent",
+    "_self",
+    "_top"
+]
+
+module.exports = {
+    RELVALUES,
+    SAFETARGETS
+}
